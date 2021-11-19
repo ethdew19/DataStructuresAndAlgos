8ee2ed7d4f9c03124a002741eab66fdcc7d7ad9e
ethdew19 <ethdew19@gmail.com>
2021-11-18 21:32:31 -0600

    more dp

---
+++ DynamicProgramming/HowSum.py
@@ -0,0 +1,7 @@
+#Write a function howSum(targetsum, numbers) that takes in a targetsum and an array of numbers
+#The Function should return an array containing any combination of elements that add up to exactly the targetsum. 
+#If there is no combination that adds up to the target, return None. If there are multiple combinations that work, return any one
+
+def howSum(targetSum, numbers):
+    
+
